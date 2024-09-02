a85d1f51de8ce08ccd93ef0e1de544beb5269fb0
nikosapac <niko.sapac@triglav.si>
2024-09-02 14:45:08 +0200

    initial commit

---
+++ src/main/java/com/triglav/digiwallboard/DigiwallboardApplication.java
@@ -0,0 +1,13 @@
+package com.triglav.digiwallboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiwallboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiwallboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triglav/digiwallboard/DigiwallboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triglav.digiwallboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiwallboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
